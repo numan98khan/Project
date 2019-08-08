4df9c01eadd7d45243f805412cb5423e60c2c362
Usama Ali <usamshiekh33>
2019-08-08 15:58:34 -0700

    android project

---
+++ app/src/main/java/com/fyp/nayapakistan/Login.java
@@ -0,0 +1,26 @@
+package com.fyp.nayapakistan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    Button button ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button = findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this , MainActivity.class));
+            }
+        });
+    }
+}
