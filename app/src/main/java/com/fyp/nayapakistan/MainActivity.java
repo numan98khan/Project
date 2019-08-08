898ab79ade2f3bd8bcd66731e2c16d1385f30dbf
Usama Ali <usamshiekh33>
2019-08-08 14:09:29 -0700

    android project

--- app/src/main/java/com/fyp/nayapakistan/MainActivity.java
+++ app/src/main/java/com/fyp/nayapakistan/MainActivity.java
@@ -3,18 +3,25 @@ package com.fyp.nayapakistan;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout ;
     ActionBarDrawerToggle actionBarDrawerToggle ;
     NavigationView navigationView ;
+    MaterialSearchView materialSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black);
+         materialSearchView = findViewById(R.id.searchview);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new MapFragment())
                 .commit();
         navigationView = findViewById(R.id.nevview);
@@ -61,5 +70,13 @@ public class MainActivity extends AppCompatActivity {
         if(actionBarDrawerToggle.onOptionsItemSelected(item))
             return true ;
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu , menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        materialSearchView.setMenuItem(item);
+        return  true ;
     }
 }
